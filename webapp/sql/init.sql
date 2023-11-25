11c1a49562f7f75af7c62ef840f5351cea11824c
tawashichan <ryotairie123@gmail.com>
2023-11-25 03:08:04 +0000

    fix reset autoincrement

--- webapp/sql/init.sql
+++ webapp/sql/init.sql
@@ -10,16 +10,3 @@ DROP TABLE livestream_tags;
 DROP TABLE livecomments;
 DROP TABLE livestreams;
 DROP TABLE users;
-
-ALTER TABLE `themes` auto_increment = 1;
-ALTER TABLE `icons` auto_increment = 1;
-ALTER TABLE `reservation_slots` auto_increment = 1;
-ALTER TABLE `livestream_tags` auto_increment = 1;
-ALTER TABLE `livestream_viewers_history` auto_increment = 1;
-ALTER TABLE `livecomment_reports` auto_increment = 1;
-ALTER TABLE `ng_words` auto_increment = 1;
-ALTER TABLE `reactions` auto_increment = 1;
-ALTER TABLE `tags` auto_increment = 1;
-ALTER TABLE `livecomments` auto_increment = 1;
-ALTER TABLE `livestreams` auto_increment = 1;
-ALTER TABLE `users` auto_increment = 1;

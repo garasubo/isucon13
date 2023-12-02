6b737d213561b5cbdb5deb38946f1f796921fa61
garasubo <garasubo@gmail.com>
2023-12-02 14:48:28 +0900

    enchou logs

--- tools/get-log.sh
+++ tools/get-log.sh
@@ -8,5 +8,5 @@ ID=$(date +%H%M%S)
 
 cd "$(dirname "$0")"
 
-ssh "$SERVER" "sudo cat /var/log/nginx/access.log" | alp ltsv --sort=sum -r -m '/api/livestream/[0-9]+$,/api/livestream/[0-9]+/enter,/api/livestream/[0-9]+/statistics,/api/livestream/[0-9]+/moderate,/api/livestream/[0-9]+/livecomment,/api/livestream/[0-9]+/reaction,/api/livestream/[0-9]+/report,/api/livestream/[0-9]+/exit,/api/livestream/[0-9]+/ngwords,/api/user/[a-zA-Z0-9]+/icon,/api/user/[a-zA-Z0-9]+/statistics,/api/user/[a-zA-Z0-9]+/theme' > "../logs/access.$ID.log"
-ssh "$DB_SERVER" "sudo mysqldumpslow -s t -t 20 /var/log/mysql/mysql-slow.log" > "../logs/mysql-slowdump.$ID.log"
+ssh "$SERVER" "sudo cat /var/log/nginx/access.log" | alp ltsv --sort=sum -r -m '/api/livestream/[0-9]+$,/api/livestream/[0-9]+/enter,/api/livestream/[0-9]+/statistics,/api/livestream/[0-9]+/moderate,/api/livestream/[0-9]+/livecomment,/api/livestream/[0-9]+/reaction,/api/livestream/[0-9]+/report,/api/livestream/[0-9]+/exit,/api/livestream/[0-9]+/ngwords,/api/user/[a-zA-Z0-9]+/icon,/api/user/[a-zA-Z0-9]+/statistics,/api/user/[a-zA-Z0-9]+/theme' > "../logs/enchou/access.$ID.log"
+ssh "$DB_SERVER" "sudo mysqldumpslow -s t -t 20 /var/log/mysql/mysql-slow.log" > "../logs/enchou/mysql-slowdump.$ID.log"
